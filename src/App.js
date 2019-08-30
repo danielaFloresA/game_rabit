93ce746758cf429e1223a12c58dddb1fa3929281
juan pablo mejia <danielafloresavila.02@gmail.com>
2019-08-30 13:42:31 -0400

    crear nuevo jugador sino existe en la coleccion jugadores
    crear nuevo jugador sino existe en la coleccion jugadores

--- src/App.js
+++ src/App.js
@@ -30,13 +30,38 @@ export class App extends React.Component{
     
     firebase.auth().signInWithPopup(provider)
         .then(result => {
-            const dbRef = firebase.firestore();
-            const collectionRef = dbRef.collection('jugadores')
-            const docRef = collectionRef.doc(result.user.email);
-            docRef.update({
-                estado : true
+            const reference = firebase.firestore().collection("jugadores").doc(result.user.email).get().then(dato => {
+                if (dato.exists) {
+                    console.log("Document data:", dato.data());
+                    const dbRef = firebase.firestore();
+                    const collectionRef = dbRef.collection('jugadores');
+                    const docRef = collectionRef.doc(result.user.email);
+                    docRef.update({
+                       estado : true
+                    });
+                } else {
+                    // doc.data() will be undefined in this case
+                    const dbRef = firebase.firestore();
+                    const collectionRef = dbRef.collection('jugadores');
+                    const docRef = collectionRef.doc(result.user.email);
+                    docRef.set({
+                        email: result.user.email,
+                        nombre: result.user.displayName,
+                        valorActual : 1,
+                        id : 0 ,//generar id aleatorio
+                        estado : true
+                    });
+                    console.log("No such document!");
+                }
+                console.log("exite email registardo",dato);
+            }).catch(function(error) {
+                console.log("Error getting document:", error);
             });
+        
+            // login update
             
+            
+            // 
             console.log(`${result.user.email} ha iniciado sesión`)
         }).catch(error => console.log(`Error ${error.code}: ${error.message}`))
     }
